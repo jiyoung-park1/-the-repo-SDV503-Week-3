0623ef74618ab89f44a5e954deb3fa412c19c517
jiyoung <jiyoung-park@live.nmit.ac.nz>
2020-03-10 09:31:28 +1300

    practice

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-let x = 10;
-let y = 5;
+const firstword = "Hello";
+const secondword = "world";
 
-console.log(x + y);
-console.log(x - y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log( x % y);
-console.log(x ** y);
+console.log(firstword + secondword);
+console.log(`${ firstword } ${ secondword }`);
+
+console.log(10 - 5);
+console.log(10  - `5`);
+console.log('10' * 5);
+console.log(10 + 10 -`5`);
+console.log(`10`/`10`);
+
+console.log(1 + 2, 3 + 4);
+console.log(5 + 6, 7 + 8);
+
+console.log("Comma operator in a  variable");
+let a = (1 +2, 3 + 4);
+console.log(a);
+
+console.log( +true);
+
+let x = 10;
+console.log(x);
+x += 10;
+console.log(x);
+x -= 10;
+console.log(x);
+x *= 10;
+console.log(x);
+x /= 10;
+console.log(x);
+x **= 10;
+console.log(x);
